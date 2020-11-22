1ef8d7a0dfef61f1aa1e7e2131620c128c9bffe8
Victor <victorgamer0720yt@gmail.com>
2020-11-22 16:09:14 -0300

    Add main script files

---
+++ HerbieV/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GTA;
+using GTA.Native;
+
+namespace HerbieV
+{
+    public class Main : Script
+    {
+        private const int ChromeWheel = 50;
+        private const int Category = 0;
+        public Main()
+        {
+            Tick += OnTick;
+        }
+
+        public void OnTick(object sender, EventArgs e)
+        {
+            var vwbeetle = World.GetAllVehicles("1963beetle");
+            // Install chrome wheel when chrome secondary paint is selected
+            foreach (var h in vwbeetle)
+            {
+                h.Mods.InstallModKit();
+                if (h.Mods.SecondaryColor == VehicleColor.Chrome &&h.Mods[VehicleModType.FrontWheel].Index == -1)
+                {
+                    //h.Mods.WheelType = VehicleWheelType.Sport;
+                    Function.Call(Hash.SET_VEHICLE_WHEEL_TYPE, h, Category);
+                    h.Mods[VehicleModType.FrontWheel].Index = ChromeWheel;
+                }
+                else if (h.Mods.SecondaryColor != VehicleColor.Chrome && h.Mods[VehicleModType.FrontWheel].Index == ChromeWheel)
+                {
+                    h.Mods[VehicleModType.FrontWheel].Index = -1;
+                }
+            }
+        }
+    }
+}
